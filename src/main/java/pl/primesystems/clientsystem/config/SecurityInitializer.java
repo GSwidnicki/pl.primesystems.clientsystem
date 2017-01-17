2e32bd7411d765ef035a91348a624523eaf37a69
Grzegorz Świdnicki <grzegorzswidnicki@MacBook-Pro-Grzegorz.local>
2017-01-17 11:37:06 +0100

    transfer encoding filter to security initializer class

--- src/main/java/pl/primesystems/clientsystem/config/SecurityInitializer.java
+++ src/main/java/pl/primesystems/clientsystem/config/SecurityInitializer.java
@@ -1,7 +1,20 @@
 package pl.primesystems.clientsystem.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.ServletContext;
 
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
+    @Override
+    protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        servletContext.addFilter("encodingFilter", characterEncodingFilter).addMappingForUrlPatterns(null, false, "/*");
+
+        super.beforeSpringSecurityFilterChain(servletContext);
+    }
 }
